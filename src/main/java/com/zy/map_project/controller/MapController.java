c2b31a908be42f749992cfde9d17f8db12a1e779
wzy <zwang489@sheffield.ac.uk>
2026-01-29 21:50:28 +0000

    fix: fix test controller

--- src/main/java/com/zy/map_project/controller/MapController.java
+++ src/main/java/com/zy/map_project/controller/MapController.java
@@ -1,8 +1,11 @@
 package com.zy.map_project.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MapController {
+
     @GetMapping("/hello")
     public String Hello(){
         return "Java Backend is Live in London";
