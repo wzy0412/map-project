1d8da22ee09e21978670c84e7bbd82425fd86f1f
wzy <zwang489@sheffield.ac.uk>
2026-01-29 20:45:55 +0000

    Migrate to Spring Boot project structure

---
+++ src/main/java/com/zy/map_project/MapProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zy.map_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapProjectApplication.class, args);
+	}
+
+}
